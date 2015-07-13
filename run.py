ccdd8a8fd55853b879bcf348bc49f2fa41b57256
HallaSurvivor <HallaSurvivor@gmail.com>
2015-07-12 20:27:05 -0400

    added intro to run.py docstring

--- run.py
+++ run.py
@@ -1,5 +1,49 @@
 """
 Run the game.
+
+MineEye created by Brian Johnson and Christopher Grossack.
+2D run and shoot game where you fall down a mineshaft.
+
+Log of stuff that happens is in the aptly named log.txt
+
+If you want the super-verbose I SEE EVERYTHING THE PROGRAM THINKS version,
+change logger.setLevel below to logging.DEBUG.
+(Fair warning - your game will run SLIGHTLY slower if you do this, as it will
+be writing to the file a LOT)
+
+Requires:
+* Python 3.4
+* PyGame
+* Pyganim (included)
+
+Much thanks to StackExchange. Seriously. We could not have made
+it this far without you and your wonderful community.
+
+Much thanks to the Speed Demos Archive forum. We asked stupid
+questions about speedrunning, and sent early builds for testing.
+The community there is great, and they helped a lot with the
+game design.
+
+Feel free to screw around with the code. It SHOULD be fairly
+legible/commented/etc. ...I hope.
+
+General outline:
+run.py - set up logger, run game loop.
+config.py - set up default config file, overridden by user settings file.
+constants.py - if you want to mess with things easily, here's where to start.
+hero.py - The hero class and subclasses.
+enemy.py - The enemy class and subclasses.
+entities.py/drops.py - Bullets, Weapons, Items, the whole lot.
+helpers.py - The start of where the magic happens. Load images, redefine sprites, make menus, etc.
+gamestates.py - The brunt of what you see that you don't realize you see.
+rooms.py - Rooms, walls, enemy calls, movement, updates, etc. If it happens during the actual GAME,
+    it probably happens here.
+
+Don't post this or modifications anywhere without emailing me first, please. (pssst. if you email me, i'll say yes <3 )
+My Email: HallaSurvivor@gmail.com
+
+
+Source: https://github.com/HallaSurvivor/mineEye
 """
 import logging
 import pygame
